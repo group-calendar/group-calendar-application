b57e4deab2ce8741f32a0cb819d5ac1f0f2d0704
developersung13 <developersung13@gmail.com>
2022-12-02 17:12:39 +0900

    [feat]: 프로젝트 방향 변경으로 인한 로그인 프레임 내 일부 레이블 수정 (#52)

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -9,16 +9,16 @@ class LoginFrame extends JFrame {
 
   private JPanel main_panel;
 
-  private JLabel lb_email;
+  private JLabel lb_groupId;
   private JLabel lb_pw;
-  private JTextField tf_email;
-  private JTextField tf_pw;
-  private JButton bt_sign_in;
-  private JButton bt_sign_up;
+  private JTextField tf_groupId;
+  private JTextField pf_pw;
+  private JButton bt_signIn;
+  private JButton bt_signUp;
 
-  private String query;
+  private int group_id;
 
-  private int user_id;
+  private String query, groupName;
 
   DBConnection dbc = new DBConnection();
 
@@ -30,37 +30,37 @@ class LoginFrame extends JFrame {
 
     main_panel = new JPanel(null);
 
-    lb_email = new JLabel("이메일");
-    lb_email.setBounds(0, 10, 80, 40);
-    lb_email.setHorizontalAlignment(JLabel.CENTER);
+    lb_groupId = new JLabel("그룹 아이디");
+    lb_groupId.setBounds(6, 10, 80, 40);
+    lb_groupId.setHorizontalAlignment(JLabel.CENTER);
     lb_pw = new JLabel("비밀번호");
     lb_pw.setBounds(0, 50, 80, 40);
     lb_pw.setHorizontalAlignment(JLabel.CENTER);
 
-    tf_email = new JTextField();
-    tf_email.setBounds(85, 10, 235, 40);
-    tf_pw = new JPasswordField();
-    tf_pw.setBounds(85, 50, 235, 40);
+    tf_groupId = new JTextField();
+    tf_groupId.setBounds(85, 10, 235, 40);
+    pf_pw = new JPasswordField();
+    pf_pw.setBounds(85, 50, 235, 40);
 
     // 로그인 버튼
-    bt_sign_in = new JButton("로그인");
-    bt_sign_in.setBounds(320, 10, 80, 80);
-    bt_sign_in.setHorizontalAlignment(JButton.CENTER);
+    bt_signIn = new JButton("접속");
+    bt_signIn.setBounds(320, 10, 80, 80);
+    bt_signIn.setHorizontalAlignment(JButton.CENTER);
 
     // 회원가입 버튼
-    bt_sign_up = new JButton("회원가입");
-    bt_sign_up.setBounds(320, 90, 80, 40);
-    bt_sign_up.setHorizontalAlignment(JButton.CENTER);
+    bt_signUp = new JButton("그룹 생성");
+    bt_signUp.setBounds(320, 90, 80, 40);
+    bt_signUp.setHorizontalAlignment(JButton.CENTER);
 
-    bt_sign_in.addActionListener(new MyActionListener());
-    bt_sign_up.addActionListener(new MyActionListener());
+    bt_signIn.addActionListener(new MyActionListener());
+    bt_signUp.addActionListener(new MyActionListener());
 
-    main_panel.add(lb_email);
-    main_panel.add(tf_email);
+    main_panel.add(lb_groupId);
+    main_panel.add(tf_groupId);
     main_panel.add(lb_pw);
-    main_panel.add(tf_pw);
-    main_panel.add(bt_sign_in);
-    main_panel.add(bt_sign_up);
+    main_panel.add(pf_pw);
+    main_panel.add(bt_signIn);
+    main_panel.add(bt_signUp);
 
     add(main_panel);
 
@@ -71,31 +71,32 @@ class LoginFrame extends JFrame {
   class MyActionListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
-      if (e.getSource() == bt_sign_in) {
+      if (e.getSource() == bt_signIn) {
         query =
-          "SELECT * FROM simple_calendar.user where email LIKE '" +
-          tf_email.getText() +
+          "SELECT * FROM simple_calendar.user where groupId LIKE '" +
+          tf_groupId.getText() +
           "' AND password LIKE '" +
-          tf_pw.getText() +
+          pf_pw.getText() +
           "'";
         System.out.println(query);
         try {
           ResultSet result = dbc.selectData(query);
           result.next();
-          user_id = Integer.parseInt(result.getString(1));
-          new SimpleCalendar(user_id);
+          group_id = Integer.parseInt(result.getString(1));
+          groupName = result.getString(5);
+          new SimpleCalendar(group_id, groupName);
           dispose();
         } catch (SQLException error) {
           System.out.println("DB 쿼리 실행 실패");
           System.out.print("사유 : " + error.getMessage());
           JOptionPane.showMessageDialog(
             null,
-            "아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.",
+            "그룹 아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.",
             "로그인 실패",
             JOptionPane.ERROR_MESSAGE
           );
         }
-      } else if (e.getSource() == bt_sign_up) {
+      } else if (e.getSource() == bt_signUp) {
         dispose();
         new SignUpFrame();
       }
